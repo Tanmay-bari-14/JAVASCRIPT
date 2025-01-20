53939f36715faaf8d253ab7e12a0550525c0c878
Tanmay-bari-14 <tanmaybari2004@gmail.com>
2025-01-20 18:10:43 +0530

    Add files via upload
    data conversion done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+// *************** PREMETIVE DATATYPES *************
+// Number       represent a numeric value or
+// String       represent a string value or ""
+// Boolean      true/false
+// BigInt
+// null         standalone value
+// undefined    undefile
+// Symbol
+
+// *************** NON_PREMETIVE DATATYPES ***********
+
+// function
+// object
+// array
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "45";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let newNumber = Number(score);
+// console.log(newNumber);
+
+// "45" => 45
+// "45a" => NaN
+// true => 1
+// false => 0
+
+let booleanValue = 1;
+
+let definedBooleanValue = Boolean(booleanValue);
+console.log(definedBooleanValue);
+
+// true => 1
+// false => 0
+// "" => false
+// "Tanmay" => true
+
+let NUM = 45;
+
+let Str = String(NUM);
+console.log(Str);
+console.log(typeof Str);
