f18e4ec1542075035a988758b1cf460bd57540d3
Tanmay-bari-14 <tanmaybari2004@gmail.com>
2025-01-21 10:28:54 +0530

    Add files via upload
    this is a first JS program
    but
    try code for reverse string.

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log("Tanmay");
+
+// ======================= REVERSE STRING ==================================
+
+let str = new String("Tanmay");
+
+console.log(str.length);
+
+for (let i = 6; i >= 0; i--) {
+  console.log(str.charAt(i));
+}
