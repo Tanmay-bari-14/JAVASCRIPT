8cbcd77bd045d711d470822298542736b5b27589
Tanmay-bari-14 <tanmaybari2004@gmail.com>
2025-01-22 14:51:29 +0530

    Add files via upload
    Objects Merge, nested objects, array of objects done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,79 @@
+// const instaUser = new Object(); // singleton object
+
+const instaUser = {};
+// console.log(instaUser);
+
+instaUser.name = "tanmay_bari_14";
+instaUser.email = "tanmaybari@demo.como";
+
+// console.log(instaUser);
+
+// ++++++++++++++++++++ NESTED OBJECTS ++++++++++++++++++++++++++
+
+const fbUser = {
+  username: "Tanmay Bari",
+  personalData: {
+    age: 21,
+    email: "test@test.com",
+  },
+};
+
+// console.log(fbUser);
+// console.log(fbUser.personalData);
+// console.log(fbUser.personalData.email);
+
+// +++++++++++++++++++ Objects Merge ++++++++++++++++++++++++++++++
+
+const num = {
+  a: 1,
+  b: 2,
+};
+const sum = {
+  a: 11,
+  b: 22,
+};
+const tum = {
+  a: 1,
+  b: 2,
+};
+
+// console.log(num, sum, tum);
+// console.log(num);
+// console.log(sum);
+// console.log(tum);
+
+const merge = { ...num, ...sum };
+// console.log(merge);
+
+const obj = Object.assign({}, num, sum);
+// console.log(obj);
+
+// ==================  =========================
+
+const data = [
+  {
+    name: "tabmsfa",
+    id: 1,
+  },
+  {
+    name: "tabmsfa",
+    id: 1,
+  },
+  {
+    name: "tabmsfa",
+    id: 1,
+  },
+];
+
+console.log(data);
+console.log(data[0].name);
+
+console.log(Object.keys(instaUser)); //Object.key('object name) gives us the key of the provided object and convert it into an array
+console.log(Object.values(instaUser)); //Object.values('object name) gives us the values of the provided object and convert it into an array
+console.log(Object.entries(instaUser)); // gives us the key value pair data in array format [key : value]
+console.log(instaUser.hasOwnProperty("isLoggedIn")); // checks the objects having a property which we provide
+
+// **************** loop on array using foreach() ******************************
+// data.forEach((element) => {
+//   console.log(element.name);
+// });
