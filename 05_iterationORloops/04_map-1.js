296b0444370a12c8fef1521cc512800086667345
Tanmay-bari-14 <tanmaybari2004@gmail.com>
2025-01-29 16:59:56 +0530

    Add files via upload
    changed a file name because of other map has been entered in the repo/ hall

---
+++ 05_iterationORloops/04_map-1.js
@@ -0,0 +1,41 @@
+const map = new Map();
+
+map.set("JS", "JAVASCRIPT");
+map.set("PY", "PHTON");
+map.set("CPP", "C++");
+
+for (const [key] of map) {
+  console.log(key);
+}
+for (const [key, val] of map) {
+  console.log(key, "= " + val);
+}
+
+for (const val of map) {
+  console.log(val);
+}
+
+// MAP => map cannot give you a duplicate value
+
+const countries = new Map();
+
+countries.set("IN", "INDIA");
+countries.set("USA", "United States of America");
+countries.set("FR", "France");
+countries.set("FR", "France");
+
+for (const [key, value] of countries) {
+  console.log(key, "stands for " + value);
+}
+
+// map cannot applicable on OBJECTS
+const user = {
+  name: "user1",
+  age: 50,
+  city: "uk",
+  gender: "male",
+};
+
+for (const [key, value] of user) {
+  console.log(key, value);
+}
